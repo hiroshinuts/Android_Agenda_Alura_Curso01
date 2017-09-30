69abdd9d9552a19a9320b17fb154a025bf635f20
Rafael Hiroshi <hiroshi_nuts@hotmail.com>
2017-09-30 00:36:54 -0300

    primeiro curso da trilha

---
+++ app/src/main/java/br/com/alura/agenda/ListaAlunosActivity.java
@@ -0,0 +1,37 @@
+package br.com.alura.agenda;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class ListaAlunosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_alunos);
+
+
+
+        String[] alunos = {"Rafael", "Hiroshi" , "Babara" , "Watanabe","Rafael", "Hiroshi" , "Babara" ,
+                "Watanabe", "Hiroshi" , "Babara" , "Watanabe", "Hiroshi" , "Babara" , "Watanabe"};
+        ListView listaAlunos = (ListView) findViewById(R.id.lista_alunos);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos);
+        listaAlunos.setAdapter(adapter);
+
+        Button novoAluno = (Button) findViewById(R.id.novo_aluno);
+        novoAluno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentVaiProFormulario = new Intent(ListaAlunosActivity.this, FormularioActivity.class);
+                startActivity(intentVaiProFormulario);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/br/com/alura/agenda/dao/AlunoDAO.java
@@ -0,0 +1,48 @@
+package br.com.alura.agenda.dao;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import br.com.alura.agenda.modelo.Aluno;
+
+/**
+ * Created by Hiro on 29/09/2017.
+ */
+
+public class AlunoDAO extends SQLiteOpenHelper{
+
+
+    public AlunoDAO(Context context) {
+        super(context, "Agenda", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "CREATE TABLE Alunos (id INTEGER PRIMARY KEY, nome TEXT NOT NULL, endereco TEXT, site TEXT, nota REAL);";
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String sql = "DROP TABLE IF EXISTS Aluno";
+        db.execSQL(sql);
+        onCreate(db);
+    }
+
+    public void insere(Aluno aluno) {
+
+        SQLiteDatabase db = getWritableDatabase();
+
+        ContentValues dados =  new ContentValues();
+        dados.put("nome", aluno.getNome());
+        dados.put("endereco", aluno.getEndereco());
+        dados.put("telefone", aluno.getTelefone());
+        dados.put("site", aluno.getSite());
+        dados.put("nota", aluno.getNota());
+
+        db.insert("Alunos", null, dados );
+
+    }
+}
